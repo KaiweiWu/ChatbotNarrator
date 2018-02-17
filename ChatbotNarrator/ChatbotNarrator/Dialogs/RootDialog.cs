7e1404277065534893116b9b79f8980415de10ab
Kaiwei Wu <kaiwei-wu@hotmail.com>
2018-02-17 16:11:58 +1300

    changed id's

--- ChatbotNarrator/ChatbotNarrator/Dialogs/RootDialog.cs
+++ ChatbotNarrator/ChatbotNarrator/Dialogs/RootDialog.cs
@@ -7,7 +7,7 @@ using Microsoft.Bot.Builder.Luis.Models;
 
 namespace ChatbotNarrator.Dialogs
 {
-    [LuisModel("206887db-a3a6-4c42-838f-c1beb71a386a", "7ef3ebbb4dec46589735b6b131ab9534", domain: "westcentralus.api.cognitive.microsoft.com")]
+    [LuisModel("9f2e0fbd-7418-4467-b67c-7957ab2c0440", "7ef3ebbb4dec46589735b6b131ab9534", domain: "westus.api.cognitive.microsoft.com")]
     [Serializable]
     public class RootLuisDialog : LuisDialog<object>
     {
